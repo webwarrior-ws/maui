cdc9851515ce32217011a48b64bbd6c042b103d5
webwarrior <reg@webwarrior.ws>
2023-03-06 15:09:22 +0100

    Core: implemented NavigationViewHandler for Gtk
    Implemented NavigationViewHandler for Gtk.
    Made changes so that NavigationViewHandler is used for
    NavigationPage in Gtk, instead of generic Page handler.
    It is incomplete at the moment. Processing of navigation
    events works, but toolbar is not implemented.
    
    Co-authored-by: Parham <parhaamsaremi@gmail.com>

--- src/Core/src/Platform/Gtk/NavigationView.cs
+++ src/Core/src/Platform/Gtk/NavigationView.cs
@@ -1,10 +1,43 @@
+using System;
+using System.Linq;
+using Gtk;
+
 namespace Microsoft.Maui.Platform
 {
 
 	public class NavigationView : Gtk.Box
 	{
-
+		Gtk.Widget? pageWidget = null;
+		IMauiContext? mauiContext = null;
+		
 		public NavigationView() : base(Gtk.Orientation.Horizontal, 0) { }
+
+		public void Connect(IStackNavigationView virtualView)
+		{
+			mauiContext = virtualView.Handler?.MauiContext;
+		}
+
+		public void Disconnect(IStackNavigationView virtualView)
+		{
+		}
+
+		public void RequestNavigation(NavigationRequest request)
+		{
+			// stack top is last
+			var page = request.NavigationStack.Last();
+			var newPageWidget = page.ToPlatform(mauiContext!);
+			if (pageWidget is null)
+			{
+				this.PackStart(newPageWidget, true, true, 0);
+			}
+			else
+			{
+				this.Remove(pageWidget);
+				this.Add(newPageWidget);
+				this.SetChildPacking(newPageWidget, true, true, 0, PackType.Start);
+			}
+			pageWidget = newPageWidget;
+		}
 
 	}
 
