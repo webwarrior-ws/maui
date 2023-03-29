65c08917ee0c68d42b2829584fc9e9385ebf1175
parham saremi <45370204+parhamsaremi@users.noreply.github.com>
2023-03-29 12:10:25 +0330

    Controls.Hosting(GTK): mappings for Frame
    Add mappers for BorderColor and CornerRadius for Frame elements
    so they can now have borders.

--- src/Controls/src/Xaml/Hosting/AppHostBuilderExtensions.cs
+++ src/Controls/src/Xaml/Hosting/AppHostBuilderExtensions.cs
@@ -26,6 +26,7 @@ using Microsoft.Maui.Controls.Compatibility.Platform.Tizen;
 #elif GTK
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Compatibility.Platform.Gtk;
+using Microsoft.Maui.Graphics.Platform.Gtk;
 #endif
 
 namespace Microsoft.Maui.Controls.Hosting
@@ -181,6 +182,7 @@ namespace Microsoft.Maui.Controls.Hosting
 			builder.Services.TryAddEnumerable(ServiceDescriptor.Transient<IMauiInitializeService, MauiControlsInitializer>());
 #endif
 			builder.RemapForControls();
+			AddDefaultMappings();
 
 			return builder;
 		}
@@ -248,6 +250,35 @@ namespace Microsoft.Maui.Controls.Hosting
 			WebView.RemapForControls();
 
 			return builder;
+		}
+
+		internal static void AddDefaultMappings()
+		{
+#if GTK
+			ContentViewHandler.Mapper.AppendToMapping(
+				"BorderColor",
+				(IContentViewHandler handler, IContentView view) =>
+				{
+					if (view is Frame frame)
+					{
+						Microsoft.Maui.Platform.ContentView contentView = (Microsoft.Maui.Platform.ContentView)handler.PlatformView;
+						contentView.SetStyleValueNode($"1px solid {frame.BorderColor.ToGdkRgba().ToString()}", contentView.CssMainNode(), "border");
+					}
+				}
+			);
+
+			ContentViewHandler.Mapper.AppendToMapping(
+				"CornerRadius",
+				(IContentViewHandler handler, IContentView view) =>
+				{
+					if (view is Frame frame)
+					{
+						Microsoft.Maui.Platform.ContentView contentView = (Microsoft.Maui.Platform.ContentView)handler.PlatformView;
+						contentView.SetStyleValueNode($"{frame.CornerRadius:f}px", contentView.CssMainNode(), "border-radius");
+					}
+				}
+			);
+#endif
 		}
 	}
 }
