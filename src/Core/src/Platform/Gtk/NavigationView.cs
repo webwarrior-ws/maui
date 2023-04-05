512ed6dbd550318b062aeeeba671062cbb9bdead
Parham <parhaamsaremi@gmail.com>
2023-04-05 18:11:46 +0330

    Core,Controls(Gtk): implement Toolbar
    Implement MauiToolbar to use in NavigationView.
    Mapped properties like Visibility and Implemented BackButton
    to be able to use HasNavigationBar and be able to navigate
    properly between pages.
    
    Co-authored-by: webwarrior-ws <reg@webwarrior.ws>

--- src/Core/src/Platform/Gtk/NavigationView.cs
+++ src/Core/src/Platform/Gtk/NavigationView.cs
@@ -1,42 +1,82 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Gtk;
 
 namespace Microsoft.Maui.Platform
 {
-
-	public class NavigationView : Gtk.Box
+	public interface IToolbarContainer
 	{
-		Gtk.Widget? pageWidget = null;
-		IMauiContext? mauiContext = null;
+		public void SetToolbar(MauiToolbar? toolbar);
+	}
+	public class NavigationView : Box, IToolbarContainer
+	{
+		MauiToolbar? _toolbar;
+		Widget? _pageWidget;
+		IMauiContext? _mauiContext;
+		IStackNavigation? _virtualView;
+		IReadOnlyList<IView> _navigationStack = new List<IView>();
 
-		public NavigationView() : base(Gtk.Orientation.Horizontal, 0) { }
+		public NavigationView() : base(Orientation.Vertical, 0)
+		{
+
+		}
 
 		public void Connect(IStackNavigationView virtualView)
 		{
-			mauiContext = virtualView.Handler?.MauiContext;
+			_mauiContext = virtualView.Handler?.MauiContext;
+			_virtualView = virtualView;
 		}
 
 		public void Disconnect(IStackNavigationView virtualView)
 		{
+			_mauiContext = null;
+			_virtualView = null;
+		}
+
+		public void SetToolbar(MauiToolbar? toolbar)
+		{
+			if (toolbar is null)
+				return;
+
+			if (_toolbar is not null)
+			{
+				_toolbar.BackButtonClicked -= NavigateBack;
+				Remove(_toolbar);
+			}
+
+			toolbar.BackButtonClicked += NavigateBack;
+			_toolbar = toolbar;
+			PackStart(_toolbar, false, true, 0);
+		}
+
+		void NavigateBack(object? sender)
+		{
+			if (_navigationStack.Count <= 1)
+			{
+				return;
+			}
+			var request = new NavigationRequest(_navigationStack.SkipLast(1).ToList(), false);
+			_virtualView?.RequestNavigation(request);
 		}
 
 		public void RequestNavigation(NavigationRequest request)
 		{
 			// stack top is last
 			var page = request.NavigationStack.Last();
-			var newPageWidget = page.ToPlatform(mauiContext!);
-			if (pageWidget is null)
+			_navigationStack = request.NavigationStack;
+			var newPageWidget = page.ToPlatform(_mauiContext!);
+			if (_pageWidget is null)
 			{
-				this.PackStart(newPageWidget, true, true, 0);
+				PackEnd(newPageWidget, true, true, 0);
 			}
 			else
 			{
-				this.Remove(pageWidget);
-				this.Add(newPageWidget);
-				this.SetChildPacking(newPageWidget, true, true, 0, PackType.Start);
+				Remove(_pageWidget);
+				Add(newPageWidget);
+				SetChildPacking(newPageWidget, true, true, 0, PackType.End);
 			}
-			pageWidget = newPageWidget;
+			_pageWidget = newPageWidget;
 		}
 
 	}
