4b8c929edba3bbd94b66cb8eb671dbd6c84ef430
webwarrior <reg@webwarrior.ws>
2024-11-21 12:31:24 +0100

    pack_mali.sh: fix indentation
    Use 4 spaces instead of 2.

--- pack_mali.sh
+++ pack_mali.sh
@@ -7,8 +7,8 @@ set -euxo pipefail
 
 # when we change PackageId we also have to change file names in buildTransitive dir
 find src/Core/src/nuget/buildTransitive -type f -name 'Microsoft.Maui.*' -print | while read -r file; do
-  newname=${file/Microsoft.Maui./Mali.}
-  mv "$file" "$newname"
+    newname=${file/Microsoft.Maui./Mali.}
+    mv "$file" "$newname"
 done
 
 cd src/BlazorWebView/src/Maui/build
@@ -17,15 +17,15 @@ mv Microsoft.AspNetCore.Components.WebView.Maui.targets Mali.AspNetCore.Componen
 cd ../../../../..
     
 echo "$PROJECTS_TO_PACK" | while read -r project; do
-  [ -z "$project" ] && continue  # Skip if empty
-  # change PackagIds to Mali.*
-  if [ "$project" = "src/BlazorWebView/src/Maui/Microsoft.AspNetCore.Components.WebView.Maui.csproj" ]; then
-    sed -i 's/<PropertyGroup>/<PropertyGroup>\n<PackageId>Mali.AspNetCore.Components.WebView<\/PackageId>/g' "$project"
-  elif grep -q "<PackageId>Microsoft.Maui" "$project"; then
-    sed -i 's/<PackageId>Microsoft.Maui/<PackageId>Mali/g' "$project"
-  else
-    sed -i -r 's/<AssemblyName>Microsoft.Maui.([^<]+)<\/AssemblyName>/<PackageId>Mali.\1<\/PackageId>\n&/g' "$project"
-  fi
-  dotnet pack "$project" --no-build --no-restore
+    [ -z "$project" ] && continue  # Skip if empty
+    # change PackagIds to Mali.*
+    if [ "$project" = "src/BlazorWebView/src/Maui/Microsoft.AspNetCore.Components.WebView.Maui.csproj" ]; then
+        sed -i 's/<PropertyGroup>/<PropertyGroup>\n<PackageId>Mali.AspNetCore.Components.WebView<\/PackageId>/g' "$project"
+    elif grep -q "<PackageId>Microsoft.Maui" "$project"; then
+        sed -i 's/<PackageId>Microsoft.Maui/<PackageId>Mali/g' "$project"
+    else
+        sed -i -r 's/<AssemblyName>Microsoft.Maui.([^<]+)<\/AssemblyName>/<PackageId>Mali.\1<\/PackageId>\n&/g' "$project"
+    fi
+    dotnet pack "$project" --no-build --no-restore
 done
 
